7ca9a9d3c4ade04f224120ae0d8ed4d32d68ceef
Ksenia Gnilitskaya <ksuha_87@mail.ru>
2022-06-17 23:42:36 +0300

    Task 4 added

---
+++ Task_4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Enter the number: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number%7==0 & number%23==0){
+    Console.Write($"{number} is a multiple of 7 and 23");
+}
+else{
+    Console.Write($"{number} is not a multiple of 7 and 23");
+}
