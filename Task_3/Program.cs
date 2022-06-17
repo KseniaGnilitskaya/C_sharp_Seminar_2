f2462e2d1f2671d625f0808117bc2a07d82f9708
Ksenia Gnilitskaya <ksuha_87@mail.ru>
2022-06-17 23:28:42 +0300

    Task 3 added

---
+++ Task_3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Enter the first number: ");
+int number1 = int.Parse(Console.ReadLine());
+
+Console.Write("Enter the second number: ");
+int number2 = int.Parse(Console.ReadLine());
+
+if (number1 % number2 == 0){
+    Console.Write($"{number1} is a multiple of {number2}");
+}
+else{
+    Console.WriteLine($"{number1} is not a multiple of {number2}");
+    Console.WriteLine(number1 % number2);
+}
