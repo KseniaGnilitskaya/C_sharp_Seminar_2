df1c14dffb0467cb3c687fd7beb06e8aaabc5687
Ksenia Gnilitskaya <ksuha_87@mail.ru>
2022-06-17 19:28:17 +0300

    Task 1 added

---
+++ Task_1/Program.cs
@@ -0,0 +1,16 @@
+﻿
+int number = new Random().Next(10, 100);
+Console.WriteLine($"Random number from 10 to 99: {number}");
+
+int digit1 = number / 10;
+int digit2 = number % 10;
+
+/* if (digit1 > digit2) {
+    Console.Write($"Max digit is {digit1}");
+}
+else {
+    Console.Write($"Max digit is {digit2}");
+} */
+
+int max = Math.Max(digit1, digit2);
+Console.Write($"Max digit is {max}");
