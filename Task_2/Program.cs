84fd029843eb8b2ed78f57c66beaa0fb1eaa43e4
Ksenia Gnilitskaya <ksuha_87@mail.ru>
2022-06-17 20:03:04 +0300

    Task 2 modified

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -1,1 +1,9 @@
 ﻿
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Random number is {number}");
+
+int FirstDigit = number / 100;
+int LastDigit = number % 10;
+
+Console.WriteLine($"Three-digit number without middle one {FirstDigit}{LastDigit}");
+
